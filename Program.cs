ab3a52109bf23931b2bac90e509798f8e2811adb
Thiago <Thiagolopespereiralp1@gmail.com>
2022-04-25 17:23:35 -0300

    finalização do exercicio

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int idade;
+
+Console.Write("Qual a sua idade: ");
+idade = Convert.ToInt32(Console.ReadLine());
+
+if (idade >= 18 && idade <= 67)
+{
+    Console.WriteLine("voçê pode doar sangue.");
+}
+
+else
+{
+    Console.WriteLine("voçê não pode doar sangue.");
+}
